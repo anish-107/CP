57e86faff4a1870c8bb37a0cb403bb12e18adf24
anish-107 <anish.k7644@gmail.com>
2024-12-30 18:13:24 +0530

    Improved Solution.java

--- Leetcode/01/Solution.java
+++ Leetcode/01/Solution.java
@@ -1,6 +1,5 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result;
 
         for(int i = 0; i < nums.length; i++) {
             for(int j = i+1; j < nums.length; j++) {
